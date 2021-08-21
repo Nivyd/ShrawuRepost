83cf3e95c94e5549b7f80a24cb2adaa1d61d7706
Nivyd <86750924+Nivyd@users.noreply.github.com>
2021-08-20 22:10:53 -0300

    Update Shrawu.lua

--- Shrawu.lua
+++ Shrawu.lua
@@ -1,6 +1,6 @@
-local p = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nivyd/Afs/main/AFS.lua"))()
+
 if game.PlaceId == 6299805723 then
-   loadstring((p))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Nivyd/Afs/main/AFS.lua", true))()
 else
- game.Players.LocalPlayer:Kick("Nao ta no anime fighters otario")
+ game.Players.LocalPlayer:Kick("Nao ta no anime fighters")
 end
